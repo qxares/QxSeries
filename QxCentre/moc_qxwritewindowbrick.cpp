69b1180d385688db0a0da7367bb3656691655f9c
qxares <ppvdbosch@hotmail.com>
2025-04-23 14:51:41 +0200

    Cleaned QxCentre build artifacts, stabilized project structure

--- QxCentre/moc_qxwritewindowbrick.cpp
+++ QxCentre/moc_qxwritewindowbrick.cpp
@@ -7,7 +7,7 @@
 *****************************************************************************/
 
 #include <memory>
-#include "qxwritewindowbrick.h"
+#include "../QxText/QxWrite/qxwritewindowbrick.h"
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
